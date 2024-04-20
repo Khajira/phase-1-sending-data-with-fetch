a629db44f57c5e7526d16b3245420dfd52483253
Benard <benardkhajira@gmail.com>
2024-04-20 11:32:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const userId = document.createElement("p");
+        userId.textContent = data.id;
+        document.body.appendChild(userId);
+      })
+      .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+      });
+  }
+  
